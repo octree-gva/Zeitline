954c0aacfff3753f5fe83a7cfd0d70397b51e89b
BafS <fabacrans@gmail.com>
2017-05-02 18:31:28 +0200

    fix: Fix wrong position for intervals

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import moment from 'moment';
 
 let conf = {
   dateRange: [
-    moment(),
-    moment().add(1, 'year'),
+    moment('2017-01-01'),
+    moment('2018-01-01'),
   ],
   timeFormat: '%B',
   ticksIntervals: 'Month', // Day, Week, Month, Year
   intervals: [
-    [moment().add(8, 'months'), moment().add(10, 'months'), 200],
-    // [moment().add(20, 'months'), moment().add(25, 'months'), 100],
-    // [moment().add(30, 'months'), moment().add(31, 'months'), 20],
+    [moment('2017-04-01'), moment('2017-06-01'), 350],
+    [moment('2017-08-01'), moment('2017-10-01'), 50],
+    // [moment().add(7, 'months'), moment().add(10, 'months'), 100],
   ],
   data: [
     {date: moment().add(10, 'hours'), label: 'test1'},
@@ -42,7 +42,7 @@ let conf = {
     {date: new Date('Jan 2030'), label: 'test16'},
   ],
   onClick: function() {
-    console.log(this);
+    console.log(this); // eslint-disable-line
     document.querySelector('body').style.background = ['#9b59b6', '#1abc9c', '#f39c12'][Math.floor(Math.random() * 3)];
     setTimeout(() => {
       document.querySelector('body').style.background = '#fff';
@@ -55,10 +55,15 @@ const margin = {top: 20, right: 20, bottom: 25, left: 30};
 const width = +svg.attr('width') - margin.left - margin.right;
 const height = +svg.attr('height') - margin.top - margin.bottom;
 
+// Sum intervals size in px
 const intervalsSum = conf.intervals.reduce((sum, interval) => sum + interval[2], 0);
 
+// Sum intervals duration
+const intervalsDateSum = conf.intervals
+  .reduce((duration, interval) => duration.add(interval[1] - interval[0]), moment.duration(0));
+
 const xMain = d3.scaleTime()
-  .domain(d3.extent(conf.dateRange))
+  .domain(d3.extent([conf.dateRange[0], conf.dateRange[1] - intervalsDateSum]))
   .range([0, width - intervalsSum]);
 
 const pivots = conf.intervals.reduce((res, interval) => {
@@ -66,8 +71,9 @@ const pivots = conf.intervals.reduce((res, interval) => {
   return {
     pivots: res.pivots.concat([
       xInterpolate + res.eaten,
-      xInterpolate + interval[2] + res.eaten,
+      xMain(interval[0]) + interval[2] + res.eaten,
     ]),
+    // Intervals previously "eaten" (in px)
     eaten: res.eaten + xInterpolate - xMain(interval[1]) + interval[2],
   };
 }, {
@@ -123,6 +129,11 @@ range.forEach((pivot, index) => {
 //     .duration(500)
 //     .ease(d3.easeLinear);
 
+/**
+ * Update data
+ *
+ * @param {object} newConf
+ */
 function updateData(newConf) {
   // Range
   if (newConf.dateRange) {
