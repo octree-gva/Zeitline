edcba6897637ee2329bcb8d163c8d37e93181e9c
BafS <fabacrans@gmail.com>
2017-05-03 12:59:10 +0200

    refactor: Factorize the code to OOP [wip]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,209 @@
 import * as d3 from 'd3';
 import moment from 'moment';
 
+const defaults = {
+  dateRange: [
+    moment('2017-01-01'),
+    moment('2018-01-01'),
+  ],
+  timeFormat: '%B',
+  ticksIntervals: 'Month',
+  intervals: [
+  ],
+  data: [
+  ],
+  onClick: function() {
+  },
+};
+
+export default class Timeline {
+
+  constructor(conf) {
+    // this.conf = conf;
+    Object.assign(this, defaults, conf);
+
+    this.init();
+  }
+
+  init() {
+    this.svg = d3.select('svg');
+    this.margin = {top: 20, right: 20, bottom: 25, left: 30};
+    this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+    this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+  }
+
+  renderAxis() {
+    // Sum intervals size in px
+    const intervalsSum = this.intervals.reduce((sum, interval) => sum + interval[2], 0);
+
+    // Sum intervals duration
+    const intervalsDateSum = this.intervals
+      .reduce((duration, interval) => duration.add(interval[1] - interval[0]), moment.duration(0));
+
+    const xMain = d3.scaleTime()
+      .domain(d3.extent([this.dateRange[0], this.dateRange[1] - intervalsDateSum]))
+      .range([0, this.width - intervalsSum]);
+
+    const pivots = this.intervals.reduce((res, interval) => {
+      const xInterpolate = xMain(interval[0]);
+      return {
+        pivots: res.pivots.concat([
+          xInterpolate + res.eaten,
+          xMain(interval[0]) + interval[2] + res.eaten,
+        ]),
+        // Intervals previously "eaten" (in px)
+        eaten: res.eaten + xInterpolate - xMain(interval[1]) + interval[2],
+      };
+    }, {
+      pivots: [],
+      eaten: 0,
+    }).pivots;
+
+    const range = [0, ...pivots, this.width];
+    const domain = this.intervals.reduce((all, int) => all.concat([int[0], int[1]]), []);
+
+    this.x = d3.scaleTime()
+      .domain([
+        d3.extent(this.dateRange)[0],
+        ...domain,
+        d3.extent(this.dateRange)[1],
+      ])
+      .range(range); // all pivots
+
+    // timeDay, timeWeek, timeMonth, timeYear
+    this.xAxis = d3.axisBottom(this.x)
+      .ticks(d3[`time${this.ticksIntervals}`])
+      .tickPadding(-5)
+      .tickSize(18);
+      // .tickFormat(d3.time.format('%Y'),)
+
+    this.svg.append('defs').append('clipPath')
+        .attr('id', 'clip')
+        .append('rect')
+        .attr('width', this.width)
+        .attr('height', this.height);
+
+    this.focus = this.svg.append('g')
+        .attr('class', 'focus')
+        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+    this.focus.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', 'translate(0,' + this.height + ')')
+        .call(this.xAxis);
+
+    // Show intervals separations
+    range.forEach((pivot, index) => {
+      this.focus
+        .append('line')
+        .attr('class', 'linear reference-line')
+          .attr('x1', +pivot)
+          .attr('x2', +pivot)
+          .attr('y1', this.height)
+          .attr('y2', this.height - 50);
+    });
+  }
+
+  /**
+   * Update data
+   *
+   * @param {object} newConf
+   */
+  update(newConf) {
+    // Range
+    if (newConf.dateRange) {
+      // x.domain(d3.extent(newConf.dateRange));
+      // x.domain(newConf.dateRange);
+    }
+
+    // Intervals
+    if (newConf.ticksIntervals) {
+      this.xAxis.ticks(d3[`time${newConf.ticksIntervals}`]);
+    }
+
+    this.focus.selectAll('circle')
+      // .exit()
+      .remove();
+
+    let that = this;
+
+    this.focus.selectAll('circle')
+      .data(newConf.data)
+      .enter()
+      .append('circle')
+      .attr('class', 'dot')
+      .attr('cx', (d) => this.x(d.date))
+      .attr('transform', function(d, i) {
+        if (i > 0) {
+          let currenty = that.x(d.date);
+          let previousy = that.x(newConf.data[i-1].date);
+          if (currenty - previousy < 15) {
+            if (newConf.data[i-1].pos) {
+              d.pos = newConf.data[i-1].pos + 1;
+            } else {
+              d.pos = 1;
+            }
+            return 'translate(' + 0 + ',' + -15 * d.pos + ')';
+          }
+        }
+      })
+      .attr('cy', 120)
+      // .on('click', (circle) => {
+      //   if (newConf.onClick) {
+      //     newConf.onClick.apply(circle);
+      //   }
+      //   d3.event.stopPropagation();
+      // })
+      .attr('r', 0)
+      .transition()
+      // .ease(d3.easeCubicOut)
+      // .duration(500)
+      .attr('r', 6);
+
+    this.focus.selectAll('circle')
+      .on('click', function(circle) {
+        d3.select(this)
+          .transition()
+          .ease(d3.easeBounceOut)
+          .duration(500)
+          .attr('r', 15)
+          .transition()
+          .duration(500)
+          .call(() => {
+            if (newConf.onClick) {
+              newConf.onClick.apply(circle);
+            }
+            // d3.event.stopPropagation();
+          })
+          .delay(500)
+          .attr('r', 6); // reset size
+        // d3.event.stopPropagation();
+    });
+
+    if (newConf.timeFormat && newConf.timeFormat !== '') {
+      this.xAxis.tickFormat((d) => d3.timeFormat(newConf.timeFormat)(d));
+    } else {
+      // Reset default format
+      this.xAxis.tickFormat(null);
+    }
+
+    // if (newConf.ticksIntervals || newConf.dateRange) {
+    this.svg.select('.axis--x')
+      .transition()
+      .duration(500)
+      .call(this.xAxis);
+    // }
+  }
+
+  render() {
+    this.renderAxis();
+  }
+}
+
+// let t = d3.transition()
+//     .duration(500)
+//     .ease(d3.easeLinear);
+
 let conf = {
   dateRange: [
     moment('2017-01-01'),
@@ -50,175 +253,9 @@ let conf = {
   },
 };
 
-const svg = d3.select('svg');
-const margin = {top: 20, right: 20, bottom: 25, left: 30};
-const width = +svg.attr('width') - margin.left - margin.right;
-const height = +svg.attr('height') - margin.top - margin.bottom;
-
-// Sum intervals size in px
-const intervalsSum = conf.intervals.reduce((sum, interval) => sum + interval[2], 0);
-
-// Sum intervals duration
-const intervalsDateSum = conf.intervals
-  .reduce((duration, interval) => duration.add(interval[1] - interval[0]), moment.duration(0));
-
-const xMain = d3.scaleTime()
-  .domain(d3.extent([conf.dateRange[0], conf.dateRange[1] - intervalsDateSum]))
-  .range([0, width - intervalsSum]);
-
-const pivots = conf.intervals.reduce((res, interval) => {
-  const xInterpolate = xMain(interval[0]);
-  return {
-    pivots: res.pivots.concat([
-      xInterpolate + res.eaten,
-      xMain(interval[0]) + interval[2] + res.eaten,
-    ]),
-    // Intervals previously "eaten" (in px)
-    eaten: res.eaten + xInterpolate - xMain(interval[1]) + interval[2],
-  };
-}, {
-  pivots: [],
-  eaten: 0,
-}).pivots;
-
-const range = [0, ...pivots, width];
-const domain = conf.intervals.reduce((all, int) => all.concat([int[0], int[1]]), []);
-
-const x = d3.scaleTime()
-  .domain([
-    d3.extent(conf.dateRange)[0],
-    ...domain,
-    d3.extent(conf.dateRange)[1],
-  ])
-  .range(range); // all pivots
-
-// timeDay, timeWeek, timeMonth, timeYear
-const xAxis = d3.axisBottom(x)
-  .ticks(d3[`time${conf.ticksIntervals}`])
-  .tickPadding(-5)
-  .tickSize(18);
-  // .tickFormat(d3.time.format('%Y'),)
-
-svg.append('defs').append('clipPath')
-    .attr('id', 'clip')
-    .append('rect')
-    .attr('width', width)
-    .attr('height', height);
-
-const focus = svg.append('g')
-    .attr('class', 'focus')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-focus.append('g')
-    .attr('class', 'axis axis--x')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
-
-// Show intervals separations
-range.forEach((pivot, index) => {
-  focus
-    .append('line')
-    .attr('class', 'linear reference-line')
-      .attr('x1', +pivot)
-      .attr('x2', +pivot)
-      .attr('y1', height)
-      .attr('y2', height - 50);
-});
-
-// let t = d3.transition()
-//     .duration(500)
-//     .ease(d3.easeLinear);
-
-/**
- * Update data
- *
- * @param {object} newConf
- */
-function updateData(newConf) {
-  // Range
-  if (newConf.dateRange) {
-    // x.domain(d3.extent(newConf.dateRange));
-    // x.domain(newConf.dateRange);
-  }
-
-  // Intervals
-  if (newConf.ticksIntervals) {
-    xAxis.ticks(d3[`time${newConf.ticksIntervals}`]);
-  }
-
-  focus.selectAll('circle')
-    // .exit()
-    .remove();
-
-  focus.selectAll('circle')
-    .data(newConf.data)
-    .enter()
-    .append('circle')
-    .attr('class', 'dot')
-    .attr('cx', (d) => x(d.date))
-    .attr('transform', function(d, i) {
-      if (i > 0) {
-        let currenty = x(d.date);
-        let previousy = x(newConf.data[i-1].date);
-        if (currenty - previousy < 15) {
-          if (newConf.data[i-1].pos) {
-            d.pos = newConf.data[i-1].pos + 1;
-          } else {
-            d.pos = 1;
-          }
-          return 'translate(' + 0 + ',' + -15 * d.pos + ')';
-        }
-      }
-    })
-    .attr('cy', 120)
-    // .on('click', (circle) => {
-    //   if (newConf.onClick) {
-    //     newConf.onClick.apply(circle);
-    //   }
-    //   d3.event.stopPropagation();
-    // })
-    .attr('r', 0)
-    .transition()
-    // .ease(d3.easeCubicOut)
-    // .duration(500)
-    .attr('r', 6);
-
-  focus.selectAll('circle')
-    .on('click', function(circle) {
-      d3.select(this)
-        .transition()
-        .ease(d3.easeBounceOut)
-        .duration(500)
-        .attr('r', 15)
-        .transition()
-        .duration(500)
-        .call(() => {
-          if (newConf.onClick) {
-            newConf.onClick.apply(circle);
-          }
-          // d3.event.stopPropagation();
-        })
-        .delay(500)
-        .attr('r', 6); // reset size
-      // d3.event.stopPropagation();
-  });
-
-  if (newConf.timeFormat && newConf.timeFormat !== '') {
-    xAxis.tickFormat((d) => d3.timeFormat(newConf.timeFormat)(d));
-  } else {
-    // Reset default format
-    xAxis.tickFormat(null);
-  }
-
-  // if (newConf.ticksIntervals || newConf.dateRange) {
-  svg.select('.axis--x')
-    .transition()
-    .duration(500)
-    .call(xAxis);
-  // }
-}
-
-updateData(conf);
+let t = new Timeline();
+t.render();
+t.update(conf);
 
 document.querySelectorAll('.interval').forEach((e) => {
   e.addEventListener('click', (e) => {
@@ -232,7 +269,7 @@ document.querySelectorAll('.interval').forEach((e) => {
       moment(),
       moment().add(1, typeInt),
     ];
-    updateData(conf);
+    t.update(conf);
   }, false);
 });
 
@@ -242,7 +279,7 @@ document.querySelector('.move-left').addEventListener('click', (e) => {
   conf.dateRange[0].subtract(diff);
   conf.dateRange[1].subtract(diff);
   // conf.dateRange[2].subtract(diff);
-  updateData(conf);
+  t.update(conf);
 }, false);
 
 document.querySelector('.move-right').addEventListener('click', (e) => {
@@ -250,5 +287,5 @@ document.querySelector('.move-right').addEventListener('click', (e) => {
 
   conf.dateRange[0].add(diff);
   conf.dateRange[1].add(diff);
-  updateData(conf);
+  t.update(conf);
 }, false);
